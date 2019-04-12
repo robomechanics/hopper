99d25ff341af582ff677e3b1cece51d6dbed9a9c
Nikolai Flowers <nikolaif399@gmail.com>
2019-04-12 11:12:40 -0400

    Updated script to hold position 0 on startup

--- odrive_script.py
+++ odrive_script.py
@@ -1,18 +1,23 @@
 import odrive
+from odrive.enums import *
 import time
 
 print("Finding an odrive...")
 odrv0 = odrive.find_any()
 print("Connected.")
 
-print("Starting calibratiom sequence on axis0...")
-odrv0.axis0.requested_state = AXIS_STATE_FULL_CALIBRATION_SEQUENCE
-while odrv0.axis0.current_state != AXIS_STATE_IDLE:
-	time.sleep(0.05)
-print("Axis0 calibrated.")
+
+#print("Starting calibratiom sequence on axis0...")
+#odrv0.axis0.requested_state = AXIS_STATE_FULL_CALIBRATION_SEQUENCE
+#while odrv0.axis0.current_state != AXIS_STATE_IDLE:
+	#time.sleep(0.05)
+#print("Axis0 calibrated.")
+print("Using prior calibration stored in configuration for Axis0.")
 
 print("Starting closed loop control...")
-my_drive.axis0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
+odrv0.axis0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
 print("Closed loop control.")
 
 print("Current bus voltage is " + str(odrv0.vbus_voltage) + "V")
+print("Moving to 3.14")
+odrv0.axis0.controller.pos_setpoint = 0
