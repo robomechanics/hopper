3a1a5e85219fb699bb13b02ef42ec407d46eb9c7
Nikolai Flowers <nikolaif399@gmail.com>
2019-04-24 13:50:00 -0400

    Fixed velocity bug in trajectory tracking and added feedforward current (need to find rotor inertia)

--- src/track_trajectory.py
+++ src/track_trajectory.py
@@ -4,9 +4,8 @@ import time
 import numpy as np
 from matplotlib import pyplot as plt
 
-CTRL_MODE_POSITION_CONTROL = 4
-CTRL_MODE_FEEDFORWARD_CONTROL = 1
 
+# Odrive setup
 print("Finding an odrive...")
 odrv0 = odrive.find_any()
 print("Connected.")
@@ -21,20 +20,33 @@ print("Starting closed loop control...")
 odrv0.axis0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
 print("Closed loop control.")
 
-
 print("Moving to position 0")
 odrv0.axis0.controller.config.control_mode = CTRL_MODE_POSITION_CONTROL
 odrv0.axis0.controller.pos_setpoint = 0
 
-time.sleep(2)
 
+# Feedforward Term
+
+
+
+time.sleep(2)
 
 t0 = time.perf_counter()
 
 t = time.perf_counter() - t0
 
-w = 2 #frequency of sine wave in hz
+
+# Trajectory features
+w_hz = 2 #frequency of sine wave in hz
+w = 2 * np.pi * w_hz
 A = 2000 #amplitude of sine wave in counts
+
+
+# Tunable properties
+I_load = 0.1 #m^2*kg
+kv = 135
+kt = 8.27/kv
+
 
 pds = np.array([])
 ps = np.array([])
@@ -43,10 +55,21 @@ ts = np.array([])
 
 while(t < 10):
 	t = time.perf_counter() - t0
-	p_desired = A * np.sin(2 * np.pi* w * t)
-	v_desired = A * np.cos(2 * np.pi* w * t)
+
+	# Compute desired values from desired trajectory
+	p_desired = A * np.sin(w * t)
+	v_desired = A * w * np.cos(w * t)
+	a_desired = -A * w^2 * np.sin(w*t)
+
+	# Transform acceleration to current
+	tau = I_load * a_desired
+	cur_desired = tau/kt
+
+	# Update setpoints 
 	odrv0.axis0.controller.pos_setpoint = p_desired
-	#odrv0.axis0.controller.vel_setpoint = v_desired
+	odrv0.axis0.controller.vel_setpoint = v_desired
+	odrv0.axis0.controller.current_setpoint = 0
+
 	p = odrv0.axis0.encoder.pos_estimate
 	#print("Position Estimate: ", p)
 	#print("Position Desired: ", p_desired)
