052b103ef61a5d35b52e0563664a09eb75ce945a
Nikolai Flowers <nikolaif399@gmail.com>
2019-05-01 19:19:54 -0400

    Hopper code structure in place

--- src/hopper.py
+++ src/hopper.py
@@ -6,9 +6,20 @@ from scipy import interpolate
 from matplotlib import pyplot as plt
 import argparse
 
+class struct(object): #include this from somewhere else
+    def __init__(self, **kwargs):
+        self.__dict__.update(kwargs)
+
 class Hopper(object):
-	def __init__(self, traj_raw, ctrl_frequency = 200, new_calibration = False): # traj_raw should be 4 x n (time s, theta rads, thetadot rad/s, torque n-m)
+	def __init__(self, raw_traj, ctrl_frequency = 200, new_calibration = False): # raw_traj should be 4 x n (time s, theta rads, thetadot rad/s, torque n-m)
 		self.odrv0 = None
+		self.ctrl_frequency = ctrl_frequency
+
+		# Motor properties
+		self.motor = struct(kt = 0.07) #this is not the exact value for kt, ballpark only
+
+		# Encoder properties
+		self.encoder = struct(offset = 0)
 
 		# Search for odrive
 		self.find_odrive()
@@ -20,13 +31,15 @@ class Hopper(object):
 		time.sleep(1)
 
 		# convert trajectory
-		self.traj = self.offline_convert_traj_to_fixed_timestep(traj_raw,ctrl_frequency)
+		traj_math_units = self.offline_convert_traj_to_fixed_timestep(traj_raw) #traj is now linearly interpolated and sampled at our control frequency, but units are still wrong
+		traj = self.offline_convert_traj_units(traj_math_units) #traj is now 4xn, with correct odrive units (seconds, counts, counts/s, amps)
 
 		# Wait for key to begin
 		print("Hopper waiting for command (enter to begin trajectory)")
 		waitkey = input()
 
 		print("Beginning trajectory...")
+		self.track_trajectory(traj)
 
 	def find_odrive(self):
 		self.odrv0 = odrive.find_any()
@@ -36,10 +49,10 @@ class Hopper(object):
 		odrv.axis0.requested_state = AXIS_STATE_FULL_CALIBRATION
 
 	def radians_to_counts(self, rads):
-		return rads * 8192/(2*np.pi)
+		return rads * 8192.0/(2*np.pi)
 
 	def counts_to_radians(self, counts):
-		return counts*2*np.pi/8192
+		return counts*2*np.pi/8192.0
 
 	def calibration(self, odrv):
 		time.sleep(1)
@@ -64,29 +77,52 @@ class Hopper(object):
 		odrv.reboot()
 
 
-	def offline_convert_traj_to_fixed_timestep(self, traj_raw, ctrl_frequency):
+	def offline_convert_traj_to_fixed_timestep(self, raw_traj):
+		#raw_traj is 4xn
 		assert(traj.shape[0] == 4)
 		tf = traj[1,-1] #last time index in traj
 
 		traj = np.array([])
 
-		tstep = 1.0/ctrl_frequency
+		tstep = 1.0/self.ctrl_frequency
 		t = 0
-		while(t < tf):
+
+		t_sampled = np.arange(0,tf,tstep)
+		Q = np.vstack(raw_traj[1,:], raw_traj[2,:], raw_traj[3,:]) # reshape our trajectory without the times for linear interpolation
+
+		f_sampled = interp1d(raw_traj[0,:],Q) # create linear interpolation function based on raw_traj timesteps
+
+		Q_sampled = f_interp(t_sampled) # sample our linear interpolation function at desired sample times
+
+		traj_math_units = np.vstack(t_sampled, Q_sampled[0,:], Q_sampled[1,:], Q_sampled[2,:]) # reshape our linearly interpolated values back into traj form
+
+		return traj_math_units
 
 
+	def offline_convert_traj_units(self, traj_math_units):
+		# row 0 goes from s        -> s           (no conversion)
+		# row 1 goes from counts   -> radians     (be mindful of encoder count offset here)
+		# row 2 goes from counts/s -> radians/s
+		# row 3 goes from n-m      -> amps
 
-			t = t + tstep
+		row0 = traj_math_units[0,:] # no conversion
+		row1 = self.counts_to_radians(traj_math_units[1,:] - self.encoder.offset)
+		row2 = self.counts_to_radians(traj_math_units[2,:])
+		row3 = traj_math_units[3,:]/self.motor.kt # TODO: experimentally determine better motor model
+
+		# TODO check that commanded currents (row3) don't excede present current limits
+
+		traj = np.vstack(row0, row1, row2, row3)
+		return traj
 
 
+	def track_trajectory(self, odrv):
+		# traj should be 4 x n [time, theta, thetadot, current] in units s, rads, rads/s and amps respectively
+
+		assert(traj.shape[0] == 4)
 
 
-
-	def track_trajectory(self, odrv, traj, tstep):
-		# traj should be 3 x n [theta, thetadot, current] in units rads, rads/s and amps respectively
-
-		assert(traj.shape[0] == 3)
-
+		tstep = 1/self.ctrl_frequency #timestep in seconds
 		n = traj.shape[1] # number of steps in simulation
 
 		
@@ -94,6 +130,9 @@ class Hopper(object):
 
 		for i in range(0, n):
 			
+			t_sampled_at = traj[0,i] #assert that this is close to our actual (measured) time at this point in the loop
+
+			# Set desired p and d as well as open loop gains
 			odrv0.axis0.controller.pos_setpoint = traj[1,i] # position in counts
 			odrv0.axis0.controller.vel_setpoint = traj[2,i] # torque in counts/s
 			odrv0.axis0.controller.current_setpoint = traj[3,i] # current in amps
